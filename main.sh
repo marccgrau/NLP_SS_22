cccc889c925dbaf622c60709063f7ce674ac2a79
marccgrau <46031196+marccgrau@users.noreply.github.com>
2022-03-19 14:23:10 +0100

    minor corrections to start up procedure

--- main.sh
+++ main.sh
@@ -1,9 +1,6 @@
-GPUS="0"
-MEMORY_LIMIT="32"
-LOCAL_CODE="/home/mcgrau/PycharmProjects/swiss2text"
-LOCAL_DATA="/data"
-./start_env.sh
+GPUS="0" MEMORY_LIMIT="32" LOCAL_CODE="/home/mcgrau/PycharmProjects/NLP_SS_22" LOCAL_DATA="/data" ./start_env.sh
 
 python3 trainConfig.py \
   --config-path=configs/models/nemo \
   --config-name=conformerCTC
+
